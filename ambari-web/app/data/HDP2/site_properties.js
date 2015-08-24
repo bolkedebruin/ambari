feceef46364734f7e228ae36a8df42f80785b7fc
Bolke de Bruin <bolke@xs4all.nl>
2015-08-24 22:25:45 +0200

    Fix typo

--- ambari-web/app/data/HDP2/site_properties.js
+++ ambari-web/app/data/HDP2/site_properties.js
@@ -2502,7 +2502,7 @@ var hdp2properties = [
   },
   {
     "id": "puppet var",
-    "name": "manage_krb5_conf",sudo yu
+    "name": "manage_krb5_conf",
     "displayName": "Manage Kerberos client krb5.conf",
     "displayType": "checkbox",
     "isOverridable": false,
